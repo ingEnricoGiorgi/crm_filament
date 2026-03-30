bc0dc9c30d5f9f3abfde4774063cb829274089ec
ingEnricoGiorgi <mo@digitalideators.com>
2026-03-30 12:54:20 +0200

    leads

---
+++ app/Filament/Resources/Leads/Schemas/LeadForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Leads\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class LeadForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('full_name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email(),
+                TextInput::make('phone')
+                    ->tel(),
+                TextInput::make('current_status')
+                    ->required()
+                    ->default('NEW'),
+            ]);
+    }
+}
