d42a2dec1aa22a22d9d78b68264970733ffc511c
ingEnricoGiorgi <mo@digitalideators.com>
2026-03-31 10:04:39 +0200

    file for translations

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -7,10 +7,10 @@ use BackedEnum;
 
 class Dashboard extends Page
 {
-    protected static ?string $navigationLabel = 'Home';
+    public static function getNavigationLabel(): string { return __('translation.title'); }
     protected static string|BackedEnum|null $navigationIcon = \Filament\Support\Icons\Heroicon::OutlinedHome;
 
-    protected static ?string $title = 'Home';
+    public function getTitle(): string { return __('translation.title'); }
 
     protected string $view = 'filament.pages.dashboard';
     protected function getHeaderWidgets(): array { return [\App\Filament\Widgets\LeadStats::class]; }
