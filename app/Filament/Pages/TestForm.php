401285dcef66cc5e2dcb2e029f52d4c8a6cc59c3
ingEnricoGiorgi <mo@digitalideators.com>
2026-04-01 12:24:26 +0200

    grid with 2 sections

--- app/Filament/Pages/TestForm.php
+++ app/Filament/Pages/TestForm.php
@@ -6,9 +6,10 @@ use Filament\Pages\Page;
 use Filament\Forms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Schemas\Components\Section;;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
+use Filament\Schemas\Components\Grid;
 
 class TestForm extends Page implements HasForms
 {
@@ -24,32 +25,38 @@ class TestForm extends Page implements HasForms
     }
 
 
-    protected function getFormSchema(): array
-    {
-        return [
-
-            Section::make('Dati utente')
-                ->schema([
-                    TextInput::make('name')->required(),
-                    TextInput::make('email')->email(),
-                ])
-                ->columns(2),
-
-            Section::make('Altre info')
-                ->schema([
-                    Select::make('role')
-                        ->options([
-                            'admin' => 'Admin',
-                            'user' => 'User',
-                        ]),
-                ]),
-        ];
-    }
-
     public function submit()
     {
         $data = $this->form->getState();
 
         dd($data); // per test
     }
+
+
+    protected function getFormSchema(): array
+    {
+        return [
+
+            Grid::make(2)
+                ->schema([
+
+                    Section::make('Dati utente')
+                        ->schema([
+                            TextInput::make('name'),
+                            TextInput::make('email'),
+                        ]),
+
+                    Section::make('Ruolo')
+                        ->schema([
+                            Select::make('role')
+                                ->options([
+                                    'admin' => 'Admin',
+                                    'user' => 'User',
+                                ]),
+                        ]),
+
+                ]),
+        ];
+    }
+
 }
